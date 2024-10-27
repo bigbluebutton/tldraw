c5bb651fb712594f2eb88ff2d3339b5abc095666
KDSBrowne <kert.browne85@gmail.com>
2024-10-27 03:02:56 +0000

    move top left tools into side menu for more screen visibility

--- packages/tldraw/src/lib/ui/components/MenuZone.tsx
+++ packages/tldraw/src/lib/ui/components/MenuZone.tsx
@@ -1,13 +1,6 @@
 import { track, useEditor } from '@bigbluebutton/editor'
 import { useBreakpoint } from '../hooks/useBreakpoint'
 import { useReadonly } from '../hooks/useReadonly'
-import { ActionsMenu } from './ActionsMenu'
-import { DuplicateButton } from './DuplicateButton'
-import { Menu } from './Menu'
-import { PageMenu } from './PageMenu/PageMenu'
-import { RedoButton } from './RedoButton'
-import { TrashButton } from './TrashButton'
-import { UndoButton } from './UndoButton'
 
 export const MenuZone = track(function MenuZone() {
 	const editor = useEditor()
@@ -18,7 +11,7 @@ export const MenuZone = track(function MenuZone() {
 	return (
 		<div className="tlui-menu-zone">
 			<div className="tlui-buttons__horizontal">
-				<Menu />
+				{/* <Menu />
 				<PageMenu />
 				{breakpoint >= 6 && !isReadonly && !editor.isInAny('hand', 'zoom') && (
 					<>
@@ -28,7 +21,7 @@ export const MenuZone = track(function MenuZone() {
 						<DuplicateButton />
 						<ActionsMenu />
 					</>
-				)}
+				)} */}
 			</div>
 		</div>
 	)
